347583ad52361473e686fcb49a87527b8c923e41
jess-alexander <alexanderjessmarie@gmail.com>
2019-08-07 15:24:49 -0500

    escaping single quotes, supporting pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  
-  config = {
-    'title':'',
-    'task-type':'',
-    // 'content':[],
-    // 'url':''
-  };
 
-  types = [
-    'blurb',
-    'iframe',
-    'quiz',
-    'questionnaire',
-  ];
+  tasktype = ['CONTENT', 'QUIZ', 'QUESTIONNAIRE'];
+  config = {};
+  pagetype=['blurb','iframe','external-link','vault'];
 
-  append(content){
-    console.log(content);
-    if(!this.config['content'])
-      {this.config['content'] = []; this.config['content'].push(content)}
-    else 
-      this.config['content'].push(content);
+  task = { 'type':''};
+
+  addPage(body: string){
+    if(!this.config['content']){
+      this.config['content'] = []; 
+      this.appendPage(body);
+    }else{
+      if(!this.config['content'].includes(this.task)){
+        this.appendPage(body)
+      }else {
+        alert("duplicate content detected");
+      }
+    } 
   }
 
+  appendPage(body){
+    if(!!body){
+      this.task['body'] = body.replace(/'/g, "\'");
+    }
+    this.config['content'].push(this.task)
+    this.task = { 'type':''};
+  }
+
+  clear(){
+    this.config = {};
+    this.task = { 'type':''};
+  }
+  
+  GOAL = {
+   content: [
+      {
+         type: 'blurb',
+         body: 'Some rich <b>text</b> crap'
+      },
+      {
+         type: 'iframe',
+         url:  'https://vault.ever....'
+      },
+      {
+         type: 'link',
+         body: 'Some rich <b>text</b> crap',
+         url:  'https://vault.ever....'
+      }
+   ]
+}
 }
